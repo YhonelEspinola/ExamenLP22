7abe39b67d98b9d4a14667dffd969f9d73063234
Yhonel Espinola <osmeryhonel890@gmail.com>
2023-09-25 14:52:24 -0500

    finished-crud

---
+++ Examen_ClI/src/main/java/com/cibertec/ExamenClIApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenClIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenClIApplication.class, args);
+	}
+
+}
